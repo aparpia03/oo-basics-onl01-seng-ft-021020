9a3e65ba27247a8e64ee65dcfbfa6910bc5320cc
Abbas Parpia <aparpia03@gmail.com>
2020-02-19 20:16:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+  def initialized(brand)
+  end 
+
+def brand=(brand)
+  @brand = brand 
+end   
+
+def brand
+  @brand 
+end   
+
+def color=(color)
+  @color = color 
+end 
+
+
+
+end   
